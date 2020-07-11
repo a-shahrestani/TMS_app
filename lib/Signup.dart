b42b64088af49d6b105b3d4b77fd368aef22dc10
Afshin Shahrestani <afshinshah77@gmail.com>
2020-07-11 10:56:38 +0430

    Our requests send FormData instead of JSON now and sign up was tested with server although there was no response but the data was stored in database

--- lib/Signup.dart
+++ lib/Signup.dart
@@ -2,48 +2,61 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 
 class SignUpPage extends StatelessWidget {
   TextEditingController nameController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
   TextEditingController emailController = TextEditingController();
   TextEditingController numberController = TextEditingController();
-  final String signupAPI = '';
+  final String signupAPI = 'http://185.205.209.236:8000/register/user/';
+  Response response;
+  var dio = Dio();
+  String _user;
+  String _pass;
+  String _email;
+  String _phoneNumber;
+  final formKey = GlobalKey<FormState>();
 
-  Future<Map<String, dynamic>> sigup(
+//  Future<Map<String, dynamic>> signup(
+  Future<String> signup(
       String username, String password, String email, String number) async {
     final Map<String, dynamic> authData = {
       "username": username,
-      "password": password,
       "email": email,
-      "number": number
+      "phone_number": number,
+      "password": password
     };
 
-    final http.Response response = await http.post(signupAPI,
-        body: json.encode(authData),
-        headers: {"Content-Type": "application/json"});
+    FormData formData = new FormData.fromMap(authData);
+//    final http.Response response = await http.post(signupAPI,
+//        body: json.encode(authData),
+//        headers: {"Content-Type": "application/json"});
+//
+//    final Map<String, dynamic> authResponseData = json.decode(response.body);
 
-    final Map<String, dynamic> authResponseData = json.decode(response.body);
+    response = await dio.post(signupAPI, data: formData);
 
-    if (response.statusCode == 400) {
-      if (authResponseData.containsKey("error")) {
-        if (authResponseData["error"] == "invalid_credentials") {
-          return {'success': false, 'message': 'Invalid User!'};
-        }
-      }
-    }
-
-    if (response.statusCode == 200) {
-      return {'success': true, 'message': 'Successfuly login!'};
-    }
-    return null;
+//    if (response.statusCode == 400) {
+////      if (authResponseData.containsKey("error")) {
+////        if (authResponseData["error"] == "invalid_credentials") {
+////          return {'success': false, 'message': 'Invalid User!'};
+////        }
+////      }
+//      return {'success': false, 'message': 'Unsuccessful SignUp!'};
+//    }
+//
+//    if (response.statusCode == 200) {
+//      return {'success': true, 'message': 'Successful SignUp!'};
+//    }
+    return response.data.toString();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Center(child: Text('سامانه مدیریت چرخ دستی')),
+          title: Center(child: Text('Trolly Management System')),
         ),
         body: Padding(
             padding: EdgeInsets.all(10),
@@ -63,60 +76,104 @@ class SignUpPage extends StatelessWidget {
                     alignment: Alignment.center,
                     padding: EdgeInsets.all(10),
                     child: Text(
-                      'اطلاعات خود را وارد کنید',
+                      'Enter your info',
                       style: TextStyle(fontSize: 20),
                     )),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: TextField(
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'نام کاربری',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  child: TextField(
-                    obscureText: true,
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'رمز عبور',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  child: TextField(
-                    controller: emailController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'ایمیل',
-                    ),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
-                  child: TextField(
-                    controller: numberController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'شماره تلفن',
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Form(
+                    key: formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        TextFormField(
+                          style: new TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.perm_identity),
+                              labelText: 'User',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(),
+                              )),
+//                          validator: (input) =>
+//                              !input.contains('@') ? 'Not a valid Email' : null,
+                          onSaved: (input) => _user = input,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.security),
+                              labelText: 'Password',
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(),
+                                  borderRadius: BorderRadius.circular(25.0))),
+                          validator: (input) => input.length < 6
+                              ? 'Password should at least be 6 characters'
+                              : null,
+                          onSaved: (input) => _pass = input,
+                          keyboardType: TextInputType.visiblePassword,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          style: new TextStyle(
+                            fontFamily: "Poppins",
+                          ),
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.email),
+                              labelText: 'Email',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(),
+                              )),
+                          validator: (input) =>
+                              !input.contains('@') ? 'Not a valid Email' : null,
+                          onSaved: (input) => _email = input,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                              icon: Icon(Icons.phone),
+                              labelText: 'Phone Number',
+                              border: OutlineInputBorder(
+                                  borderSide: BorderSide(),
+                                  borderRadius: BorderRadius.circular(25.0))),
+//                          validator: (input) => input.length < 6
+//                              ? 'Password should at least be 6 characters'
+//                              : null,
+                          onSaved: (input) => _phoneNumber = input,
+                          keyboardType: TextInputType.visiblePassword,
+                        ),
+                      ],
                     ),
                   ),
                 ),
                 Container(
                     height: 50,
-                    padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+                    padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
                     child: RaisedButton(
                       textColor: Colors.white,
                       color: Colors.blue,
-                      child: Text('ثبت نام'),
+                      child: Text('SignUp'),
                       onPressed: () {
 //                        print(nameController.text);
 //                        print(passwordController.text);
+                        if (formKey.currentState.validate()) {
+                          formKey.currentState.save();
+                          print(_user + _pass + _email + _phoneNumber);
+//                        print(_sigin(_user, _pass).toString());
+                          print(signup(_user, _pass, _email, _phoneNumber));
+//                          Navigator.pushReplacementNamed(context, '/MapPage');
+                        }
                       },
                     )),
                 Container(
@@ -125,7 +182,7 @@ class SignUpPage extends StatelessWidget {
                     FlatButton(
                       textColor: Colors.blue,
                       child: Text(
-                        'ورود',
+                        'Login',
                         style: TextStyle(fontSize: 20),
                       ),
                       onPressed: () {
@@ -133,10 +190,10 @@ class SignUpPage extends StatelessWidget {
                         Navigator.pushReplacementNamed(context, '/LoginPage');
                       },
                     ),
-                    Text('از قبل حساب کاربری دارید؟'),
+                    Text('Do you have an account?'),
                   ],
                   mainAxisAlignment: MainAxisAlignment.center,
-                ))
+                )),
               ],
             )));
   }
